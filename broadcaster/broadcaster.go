54f491e4fd47edd5cd1ce93849eb650bb9fcd62e
NicoGGG <nicolas.guelfi@achilio.com>
2022-04-28 17:39:36 +0200

    some logs

--- broadcaster/broadcaster.go
+++ broadcaster/broadcaster.go
@@ -118,6 +118,7 @@ func (b *Broadcaster) validatePubSubJwt(next http.Handler) http.Handler {
 		if err != nil {
 			e := fmt.Sprintf("Invalid Token: %v", err)
 			log.Println(e)
+			log.Println(payload.Audience)
 			http.Error(w, e, http.StatusBadRequest)
 			return
 		}
